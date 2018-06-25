4a3d24ed998ed6e7f948ec48f88a0385a9560db0
Rikuhashimoto <rikuh>
2018-06-25 13:40:35 +0900

    子供の設定

---
+++ src/components/pages/child.vue
@@ -0,0 +1,66 @@
+<template>
+<div id = "form" class="modal-card" style="width: auto">
+    <form>
+ <footer class="modal-card-foot">
+    <h1>子ども　登録</h1>
+     </footer>
+    <section class="modal-card-body">
+   <b-field label="Name">
+                    <b-input
+                            type="text"
+                            v-model="name"
+                            placeholder="child name"
+                            required>
+                    </b-input>
+                </b-field>
+     </section>
+
+    <section class="modal-card-body">
+    <b-field label="Select a birthday">
+        <b-datepicker
+            placeholder="Click to select..."
+            icon="calendar-today"
+            v-model="month"
+            >
+        </b-datepicker>
+           </b-field>     
+           </section> 
+
+     <section class="modal-card-body">
+    <b-radio v-model="radio"
+                native-value=0>
+                男
+            </b-radio>
+            <b-radio v-model="radio"
+                native-value=１>
+                女
+            </b-radio>
+     </section>
+
+     <footer class="modal-card-foot">
+    <button class="button" type="button"  @click="onclick">登録</button>
+     </footer>
+    </form>
+     <p v-if="month">{{month}}</p>
+    <under-tab :index=2 ></under-tab>
+</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                name : "",
+                radio: null,
+                 month: null,
+            }
+        } ,
+        methods:{
+      onclick:()=>{
+        month = month.substr( 0, 10 );
+      }
+           }
+    }
+</script>
+
+
