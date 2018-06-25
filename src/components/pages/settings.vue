5ebf2c67f094afc464193d488526fa64d5d769d6
Rikuhashimoto <rikuh>
2018-06-25 14:23:30 +0900

    設定画面

--- src/components/pages/settings.vue
+++ src/components/pages/settings.vue
@@ -1,45 +1,23 @@
 <template>
+<div>
     <button class="button" type="button" @click="child">子ども登録</button>
     <button class="button" type="button" @click="logout">ログアウト</button>
+    <under-tab :index=2 ></under-tab>
+</div>
 </template>
 
 <script>
-    import axios from 'axios'
-    import http from '../../service/service'
-    import auth from '../../service/auth'
     export default {
         data() {
             return {
-                name: "",
-                password: "",
-                err: ""
             }
         },
         methods:{
             child(){
-                http.signup(this.name, this.password)
-                .then((response)=> {
-                    console.log(response.data.success);
-                    this.signin();
-                }) 
-                .catch((error)=>{
-                    //   console.log(error.response.data);
-                    this.err = error.response.data.error;
-                });
+            this.$router.push({ path: '/child' });
             },
             logout(){
-                http.signin(this.name, this.password)
-                .then((response) => {
-                    console.log("then");
-                    console.log(response.data.token);
-                    auth.SetToken(response.data.token);
-                    this.$router.push({ path: '/logout' });
-                })
-                .catch((error) => {
-                console.log("catch");
-                     console.log(error.response.data);
-                     this.err = error.response.data.error;
-                });
+               
             }
         }
     }
