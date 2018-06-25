1e9c3fcf97322b1a3c7270f6ba36ebbf32e51c28
Rikuhashimoto <rikuh>
2018-06-25 18:01:06 +0900

    子ども一覧取得

--- src/components/pages/childlist.vue
+++ src/components/pages/childlist.vue
@@ -1,9 +1,26 @@
 <template>
+<div>
         <under-tab :index=2 ></under-tab>
+
+</div>
 </template>
 
 <script>
+import http from "../../service/service";
+import axios from "axios";
+import auth from "../../service/auth";
+
 export default {
-    
-}
+  created: function() {
+    http
+      .getchild()
+      .then(response => {
+        console.log(response);
+        this.lis = response.data;
+      })
+      .catch(function(error) {
+        console.log(error.response);
+      });
+  }
+};
 </script>
--- src/service/service.js
+++ src/service/service.js
@@ -1,53 +1,55 @@
 import axios from 'axios'
-import auth  from './auth'
+import auth from './auth'
 URL = 'https://caseteambserver-nrgwuntsge.now.sh/';
 const api = axios.create({
     baseURL: URL, // バックエンドB のURL:port を指定する
     headers: {
-      'ContentType': 'application/json',
-      'Authorization': auth.GetToken()
+        'ContentType': 'application/json',
+        'Authorization': auth.GetToken()
     },
-    responseType: 'json'  
-  });
+    responseType: 'json'
+});
 
-class Http{
-    constructor(){
-    }
-    signin(name, pass){
-        return  api.post('signin',{
+class Http {
+    constructor() {}
+    signin(name, pass) {
+        return api.post('signin', {
             name,
             pass
         })
     }
-    signup(name, pass){
-        return  api.post('signup',{
+    signup(name, pass) {
+        return api.post('signup', {
             name,
             pass
         })
         alert("Siginup");
     }
-    getpin(){
-        return  api.post('goal/button')
-  }
-    getid(){
-    return  api.get('goal/button')
-  }
-  
-    addgoal(button_id,goal){
-        return api.post('goal/goal',{
-         button_id,
-         goal
-        
-    })
-  }
-  addchild(nickname,birthday,sex){
-    return api.post('child',{
-        nickname,
-        birthday,
-        sex
-       
-   })
-  }
+    getpin() {
+        return api.post('goal/button')
+    }
+    getid() {
+        return api.get('goal/button')
+    }
+
+    addgoal(button_id, goal) {
+        return api.post('goal/goal', {
+            button_id,
+            goal
+
+        })
+    }
+    addchild(nickname, birthday, sex) {
+        return api.post('child', {
+            nickname,
+            birthday,
+            sex
+
+        })
+    }
+    getchild() {
+        return api.get('child')
+    }
 }
 var http = new Http()
 export default http;
