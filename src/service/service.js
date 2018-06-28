4e10b0d486ad53438b7da4f6ce435824946a21e8
Rikuhashimoto <rikuh>
2018-06-28 17:22:25 +0900

    変更

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import auth from './auth'
-URL = 'https://caseteambserver-lojbesjryh.now.sh/';
+URL = 'https://caseteambserver-zlttuuhtuv.now.sh';
 const api = axios.create({
     baseURL: URL, // バックエンドB のURL:port を指定する
     headers: {
